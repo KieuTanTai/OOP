36990d6d6ae8f6a2dba4bc522456fbc1941d7cba
KieuTanTai <kieutantai2x@gmail.com>
2024-10-05 11:09:29 +0700

    remove test folder

--- Test/src/main/java/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main (String[] args) {
-        System.out.println("truoc do cac ban phai tai nvm");
-        System.out.println("a thoi dua link cac ban doc la hieu lien :3");
-    }
-}

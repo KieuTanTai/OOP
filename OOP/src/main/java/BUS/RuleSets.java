77690c7e68ceb52d33b771c463d6c11a5f5c4a22
lil-hi3N <madanhhien7@gmail.com>
2024-11-04 18:46:21 +0700

    push code bill, billdetails lan 2 tai lan 1 lam sai

--- OOP/src/main/java/BUS/RuleSets.java
+++
@@ -1,9 +0,0 @@
-package BUS;
-
-public interface RuleSets {
-     public abstract void add(Object objectId);
-     public abstract int find(String id); //return index
-     public abstract void search(String id);
-     public abstract void remove(String id);
-     public abstract void edit(String id);
-}

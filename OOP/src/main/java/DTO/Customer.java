976bd8c4e3036b52af3d455a211b39dcbac0da16
nghuy2002 <thanhhuylenguyen2002@gmail.com>
2024-11-10 21:18:59 +0700

    nop 3 class

---
+++ OOP/src/main/java/DTO/Customer.java
@@ -0,0 +1,47 @@
+package DTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Customer extends person {
+    // Thuộc tính (Fields) riêng của Customer
+    private String address;
+    private BigDecimal point;
+
+    // Constructor
+    public Customer(String id, String firstName, String lastName, LocalDate birthday, String phone,
+                    String address, BigDecimal point) {
+        super(id, firstName, lastName, birthday, phone); // Gọi constructor của lớp cha (Person)
+        this.address = address;
+        this.point = point;
+    }
+
+    // Getter và Setter cho address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Getter và Setter cho point
+    public BigDecimal getPoint() {
+        return point;
+    }
+
+    public void setPoint(BigDecimal point) {
+        this.point = point;
+    }
+
+    // Triển khai phương thức trừu tượng từ lớp cha
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + getId());
+        System.out.println("Name: " + getFirstName() + " " + getLastName());
+        System.out.println("Birthday: " + getBirthday());
+        System.out.println("Phone: " + getPhone());
+        System.out.println("Address: " + address);
+        System.out.println("Points: " + point);
+    }
+}

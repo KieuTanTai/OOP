0dc801032eda951108bd4efa8046b7c26151245a
KieuTanTai <kieutantai2x@gmail.com>
2024-11-13 18:08:39 +0700

    config validate

--- OOP/src/main/java/App.java
+++ OOP/src/main/java/App.java
@@ -1,24 +1,23 @@
 import util.Validate;
 import java.io.IOException;
 import java.util.Scanner;
-import BUS.TypesBUS;
 
 public class App {
     public static void main(String[] args) throws IOException {
         // Menu app = new Menu();
         Scanner input = new Scanner(System.in);
-        String temp;
+        String tempI;
 
-        TypesBUS listTypes = new TypesBUS();
-        listTypes.readFile();
         do {
             System.out.print("test validate: ");
-            temp = input.nextLine().trim();
-            if (!Validate.checkName(temp)) {
-                System.out.println("error name!");
-                temp = "";
+            tempI = input.nextLine().trim();
+            if (!Validate.validateID(tempI)) {
+                System.out.println("error bigDecimal!");
+                tempI = "";
             }
-        } while (temp.isEmpty());
+        } while (tempI.isEmpty());
+        System.out.println(tempI);
+
         input.close();
 
     }

ad50c00ab2c8984e3a47118bda31378dd8df81cc
Nhi Chan <129971590+ntun-276@users.noreply.github.com>
2024-11-18 21:50:53 +0700

    Nhi (#4)
    * Delete .idea directory
    
    * Delete OOP/.idea directory
    
    * test
    
    * lam sale event
    
    * nhap xuat sua
    
    * them nhap xuat update
    
    ---------
    
    Co-authored-by: Kieu_Tai <129686013+KieuTanTai@users.noreply.github.com>

---
+++ OOP/src/main/java/DTO/SaleEvents.java
@@ -0,0 +1,123 @@
+package DTO;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class SaleEvents {
+    private String saleEvId;
+    private String saleEvName;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private SaleEventsDetail detail;
+    Scanner sc = new Scanner(System.in);
+
+    // constructor
+    public SaleEvents() {
+    }
+
+    public SaleEvents(String saleEvId, String saleEvName, String description, LocalDate startDate, LocalDate enđate, SaleEventsDetail detail) {
+        this.saleEvId = saleEvId;
+        this.saleEvName = saleEvName;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = enđate;
+        this.detail = detail;
+    }
+
+    public SaleEvents(String saleEvId, String saleEvName, String description, LocalDate startDate, LocalDate enđate) {
+        this.saleEvId = saleEvId;
+        this.saleEvName = saleEvName;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = enđate;
+    }
+
+    public void nhap() {
+        System.out.println("Nhập sale events id:");
+        this.saleEvId = sc.nextLine();
+
+        System.out.println("Nhập sale events name:");
+        this.saleEvName = sc.nextLine();
+
+        System.out.println("Nhập description:");
+        this.description = sc.nextLine();
+
+        System.out.println("Nhập start date:");
+        this.startDate = LocalDate.parse(sc.next());
+
+        System.out.println("Nhập end date:");
+        this.endDate = LocalDate.parse(sc.next());
+
+        this.detail.setSaleEvId(this.saleEvId);
+        this.detail.nhap();
+    }
+
+    public void xuat(){
+        System.out.println("sale events id là:" + this.saleEvId);
+        System.out.println("sale events name là:" + this.saleEvName);
+        System.out.println("description là:" + this.description);
+        System.out.println("star date là:" + this.startDate);
+        System.out.println("end date là:" + this.endDate);
+    }
+
+    public void xuatGomDetail(){
+        System.out.println("sale events id là:" + this.saleEvId);
+        System.out.println("sale events name là:" + this.saleEvName);
+        System.out.println("description là:" + this.description);
+        System.out.println("star date là:" + this.startDate);
+        System.out.println("end date là:" + this.endDate);
+        this.detail.xuat();
+    }
+
+    // getter
+    public String getSaleEvId() {
+        return saleEvId;
+    }
+
+    public String getSaleEvName() {
+        return saleEvName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public SaleEventsDetail getDetail() {
+        return detail;
+    }
+
+    // setter
+    public void setSaleEvId(String saleEvId) {
+        this.saleEvId = saleEvId;
+    }
+
+    public void setSaleEvName(String saleEvName) {
+        this.saleEvName = saleEvName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(LocalDate enđate) {
+        this.endDate = enđate;
+    }
+
+    public void setDetail(SaleEventsDetail detail) {
+        this.detail = detail;
+    }
+
+}
---
+++ OOP/src/main/java/DTO/SaleEventsDetail.java
@@ -0,0 +1,94 @@
+package DTO;
+
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class SaleEventsDetail {
+    private String saleEvId;
+    private String promoCode;
+    private BigDecimal minPrice;
+    private BigDecimal discount;
+    private BigDecimal maxPriceDiscount;
+    Scanner sc = new Scanner(System.in);
+
+    // constructor rong
+    public SaleEventsDetail() {
+    }
+
+    public SaleEventsDetail(String saleEvId, String promoCode, BigDecimal minPrice, BigDecimal discount, BigDecimal maxPriceDiscount) {
+        this.saleEvId = saleEvId;
+        this.promoCode = promoCode;
+        this.minPrice = minPrice;
+        this.discount = discount;
+        this.maxPriceDiscount = maxPriceDiscount;
+    }
+
+    public void nhap(){
+        System.out.println("Nhập promo code:");
+        this.promoCode = sc.nextLine();
+
+        System.out.println("Nhập min price:");
+        this.minPrice = sc.nextBigDecimal();
+
+        System.out.println("Nhập discount:");
+        this.discount = sc.nextBigDecimal();
+
+        System.out.println("Nhập max price discount:");
+        this.maxPriceDiscount = sc.nextBigDecimal();
+    }
+
+    public void xuat(){
+        System.out.println("promo code là:" + this.promoCode);
+        System.out.println("min price là:" + this.minPrice);
+        System.out.println("discount là:" + this.discount);
+        System.out.println("max price discount là:" + this.maxPriceDiscount);
+    }
+
+
+    // getter
+    public String getSaleEvId() {
+        return saleEvId;
+    }
+
+    public String getPromoCode() {
+        return promoCode;
+    }
+
+    public BigDecimal getMinPrice() {
+        return minPrice;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public BigDecimal getMaxPriceDiscount() {
+        return maxPriceDiscount;
+    }
+
+    // setter
+    public void setSaleEvId(String saleEvId) {
+        this.saleEvId = saleEvId;
+    }
+
+    public void setPromoCode(String promoCode) {
+        this.promoCode = promoCode;
+    }
+
+    public void setMinPrice(BigDecimal minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public void setMaxPriceDiscount(BigDecimal maxPriceDiscount) {
+        this.maxPriceDiscount = maxPriceDiscount;
+    }
+
+
+
+
+}

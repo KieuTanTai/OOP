d2809f4e4a5c803ad8df44a8539bde4afeafd2d0
KieuTanTai <kieutantai2x@gmail.com>
2024-12-10 15:11:08 +0700

    add methods login for Employee

--- src/main/java/BUS/EmployeesBUS.java
+++ src/main/java/BUS/EmployeesBUS.java
@@ -703,11 +703,8 @@ public class EmployeesBUS implements IRuleSets {
         int index = find(userName);
         if (index == -1)
             return false;
-            if (!employeesList[index].getRole().equals("Manager") || !employeesList[index].checkPassword(password)) {
-                System.out.println(employeesList[index].getPassword());
-                System.out.println(employeesList[index].checkPassword(password));
+        if (!employeesList[index].getRole().equals("Manager") || !employeesList[index].checkPassword(password))
             return false;
-        }
         return true;
     }
 
